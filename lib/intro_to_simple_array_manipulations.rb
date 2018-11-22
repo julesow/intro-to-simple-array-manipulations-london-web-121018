9a28a538d005315bdea907c1b395da17d4c9d3da
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-11-22 22:08:39 +0000

    automatically backed up by learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,40 @@
+def using_push(array,s)
+  array.push(s)
+end
+
+def using_unshift(array,s)
+  array.unshift(s)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array,array1)
+  array.concat(array1)
+end 
+
+def using_insert(array,element)
+  array.insert(4,element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_uniq(array)
+  array.uniq
+end
